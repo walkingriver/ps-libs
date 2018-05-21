e206241847a82e26c2d2c7097238db30cdc4ea85
Mike Callaghan <mikecallaghan67@gmail.com>
2018-05-21 08:36:29 -0400

    Update word component with interface

--- src/app/components/random-word/random-word.component.ts
+++ src/app/components/random-word/random-word.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { WordService } from '../../services/word.service';
+import { IWord } from '../../iword';
+import { WordTypes } from '../../word-types.enum';
 
 @Component({
   selector: 'app-random-word',
-  template: '<span *ngIf="word">{{word}}</span><span *ngIf="!word"><strong><em>{{type}}</em></strong></span>',
+  template: `<span *ngIf="word">
+  <strong><em>{{word.label}}</em></strong>
+  </span>
+  <span *ngIf="!word"><strong><em>{{type}}</em></strong>
+  </span>`,
   styles: []
 })
 export class RandomWordComponent implements OnInit {
-  @Input() type: string;
-  word: string;
+  @Input() type: WordTypes;
+  word: IWord;
 
   constructor(public wordsService: WordService) { }
 
